9b642ff3558b8da2d2c3a16f50c4d4bfa1e6ca8b
Sergey Desyatkov <Desyatkov@gmail.com>
2017-07-24 13:21:25 +0300

    new state

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  webserver = require('gulp-webserver'),
-  postcss = require('gulp-postcss'),
+var gulp       = require('gulp'),
+  gutil        = require('gulp-util'),
+  webserver    = require('gulp-webserver'),
+  postcss      = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
-  precss = require('precss'),
-  cssnano = require('cssnano'),
-  animation = require('postcss-animation'),
-  source = 'process/css/',
-  sourcejs = 'process/js/',
-  dest = 'builds/postcss/',
-  babel = require('gulp-babel'),
-  browserify = require('gulp-browserify');
-  
+  precss       = require('precss'),
+  cssnano      = require('cssnano'),
+  animation    = require('postcss-animation'),
+  babel        = require('gulp-babel'),
+  browserify   = require('gulp-browserify'),
+  oldie        = require('oldie'),
+  uglify       = require('gulp-uglify'),
+  source       = 'process/css/',
+  sourcejs     = 'process/js/',
+  dest         = 'builds/postcss/';
+
 gulp.task('html', function() {
   gulp.src(dest + '*.html');
 });
@@ -28,15 +30,28 @@ gulp.task('css', function() {
   .pipe(gulp.dest(dest + 'css'));
 });
 
+gulp.task('cssIE8', function() {
+  gulp.src(source + 'style.css')
+  .pipe(postcss([
+    precss(),
+    animation(),
+    autoprefixer(),
+    cssnano(),
+    oldie()
+  ]))
+  .on('error', gutil.log)
+  .pipe(gulp.dest(dest + 'cssOldie'));
+});
+
 gulp.task('script', function() {
     gulp.src(sourcejs + 'app.js')
         .pipe(browserify({
-          insertGlobals : true,
-          debug : !gulp.env.production
+          insertGlobals : true
         }))
         .pipe(babel({
             presets: ['es2015','stage-0']
         }))
+        .pipe(uglify())
         .pipe(gulp.dest(dest + 'js'));
 });
 
@@ -54,4 +69,4 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('default', ['html', 'css','script', 'webserver', 'watch']);
+gulp.task('default', ['html', 'css', 'cssIE8','script', 'webserver', 'watch']);
--- process/js/app.js
+++ process/js/app.js
@@ -1,27 +1,46 @@
-var $ = require("jquery");
+const $ = require('jquery');
 
-const $productItem = $('.product-item')
+const $productItem = $('.product-item');
+const $additionalContent = $('.product-additional-content');
+function placeBox(el) {
+  el.each(function (i, el) {
+    const elem   = $(this).find('.product-additional-content');
+    const $width = $(this).width();
+    const $pos   = $(this).position();
 
-function placeBox(el){
-    el.each(function( i, el ) {
-        const elem = $(this).find('.product-additional-content');
-        const $width = $( this ).width();
-        const $pos = $( this ).position();
-
-        elem.width($width)
-            .css({left: $pos.left });
-    });
+    elem.width($width).css({ left: $pos.left });
+  });
 }
 
+//on page init
 placeBox($productItem);
 
-$( window ).resize(function() {
+//on page resize
+$(window).resize( () => placeBox($productItem) );
+
+//on product-list scroll
+$('.product-list').scroll( () => {
+  $additionalContent.removeClass('animate-card');
   placeBox($productItem);
 });
 
-$( ".product-list" ).scroll(function() {
-  placeBox($productItem);
-});
+$('.product-item').mouseover(function () {
+  event.stopPropagation();
+  $(this).find('.product-additional-content').addClass('animate-card');
+})
+
+$('.product-item').mouseout( () => $additionalContent.removeClass('animate-card') );
 
 
+$('.container, #why-us-part').click( 
+  () => $additionalContent.removeClass('animate-card') 
+);
 
+// review
+const $reviewText = $('.review > .content .desc');
+
+$reviewText.each(function(){
+  $(this).text().length > 110
+         ? $(this).text( $(this).text().substring(0, 110).concat('...') ) 
+         : false;
+})
